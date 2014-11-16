b4c1d551359252e1f17181777e22961e4ba5450c
root <bderstine@gmail.com>
2014-11-16 12:58:50 +0000

    no idea

--- setup.py
+++ setup.py
@@ -1,2 +1,18 @@
 #!venv/bin/python
+#This will need to ask for values and then update and deploy template files with those values.
 
+#check for /srv/[domain]
+
+#api.wsgi.template -> api.wsgi
+# [domain]
+
+#config.py.template -> config.py
+# [appname]
+
+#virtualhosts/template.com.conf -> [domain].com.conf
+# [domain]
+# [appname]
+
+# $ ln -s /etc/apache2/virtualhosts/[domain].conf /srv/[domain]/virtualhosts/[domain].conf
+# $ a2ensite [domain].conf
+# $ service apache2 reload
