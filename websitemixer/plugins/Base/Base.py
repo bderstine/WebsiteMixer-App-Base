70f5cba12a9ff7668b8f86463fc98677cea68d17
Chuck Clift <chuckcclift@gmail.com>
2016-10-15 21:47:14 -0400

    made Base.py valid pep8

--- websitemixer/plugins/Base/Base.py
+++ websitemixer/plugins/Base/Base.py
@@ -8,6 +8,7 @@ from websitemixer import app
 from websitemixer.models import *
 from websitemixer.functions import *
 
+
 @app.route('/', defaults={'tag': None})
 @app.route('/tag/<tag>/')
 def home(tag):
@@ -20,25 +21,28 @@ def home(tag):
 
     s = getSettings()
     if tag:
-        blogData = Post.query.filter(Post.tags.like('%'+tag+'%')).order_by(Post.date.desc()).all()
+        blogData = Post.query.filter(Post.tags.like('%'+tag+'%')).\
+                       order_by(Post.date.desc()).all()
     else:
         blogData = Post.query.order_by(Post.date.desc()).limit(5)
-    return render_template(s['theme']+'/index.html',blogData=blogData,s=s)
+    return render_template(s['theme'] + '/index.html', blogData=blogData, s=s)
 
-@app.route('/login/',methods=['GET','POST'])
+
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
     s = getSettings()
     error = None
     if request.method == 'GET':
-        return render_template(s['theme']+'/login.html',s=s)
+        return render_template(s['theme'] + '/login.html', s=s)
     username = request.form['username']
     password = request.form['password']
-    registered_user = User.validate(username,password)
+    registered_user = User.validate(username, password)
     if registered_user is False:
         return redirect('/login/')
     u = User.query.filter_by(username=username).first()
     login_user(u)
     return redirect('/admin/')
+
 
 @app.route("/logout/")
 def logout():
@@ -46,15 +50,19 @@ def logout():
     logout_user()
     return redirect('/')
 
+
 def make_external(url):
     return urljoin(request.url_root, url)
 
-@app.route('/feed/',defaults={'tag': None})
+
+@app.route('/feed/', defaults={'tag': None})
 @app.route('/feed/<tag>/')
 def recent_feed(tag):
-    feed = AtomFeed('Recent Articles', feed_url=request.url, url=request.url_root)
+    feed = AtomFeed('Recent Articles', feed_url=request.url,
+                    url=request.url_root)
     if tag:
-        articles = Post.query.filter(Post.tags.like('%'+tag+'%')).order_by(Post.date.desc()).limit(15).all()
+        articles = Post.query.filter(Post.tags.like('%'+tag+'%'))\
+                       .order_by(Post.date.desc()).limit(15).all()
     else:
         articles = Post.query.order_by(Post.date.desc()).limit(15).all()
     for article in articles:
@@ -66,6 +74,7 @@ def recent_feed(tag):
                  published=article.date)
     return feed.get_response()
 
+
 @app.route('/<path:path>')
 def content(path):
     s = getSettings()
@@ -73,24 +82,28 @@ def content(path):
     # check if path=slug, if yes show
     postData = Post.query.filter_by(slug=slug).first()
     if postData is not None:
-        return render_template(s['theme']+'/post.html',postData=postData,s=s)
+        return render_template(s['theme']+'/post.html', postData=postData, s=s)
     # check if path=slug, if yes show
     pageData = Page.query.filter_by(slug=slug).first()
     if pageData is not None:
-        return render_template(s['theme']+'/page.html',pageData=pageData,s=s)
+        return render_template(s['theme'] + '/page.html',
+                               pageData=pageData,
+                               s=s)
     # else show 404
-    return render_template(s['theme']+'/404.html',s=s), 404
+    return render_template(s['theme'] + '/404.html', s=s), 404
 
 #######################################################################
 # Error handlers
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     s = getSettings()
-    return render_template(s['theme']+'/404.html',s=s), 404
+    return render_template(s['theme'] + '/404.html', s=s), 404
+
 
 @app.errorhandler(500)
 def internal_error(error):
     s = getSettings()
     db.session.rollback()
-    return render_template(s['theme']+'/500.html',s=s,error=error), 500
+    return render_template(s['theme'] + '/500.html', s=s, error=error), 500
