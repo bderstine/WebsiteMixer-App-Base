85654ef300b479c936a753453d85a2f153b1ed9f
bderstine <brad@bizzartech.com>
2018-02-08 21:56:57 +0000

    fix config.py

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-DEBUG = False
+DEBUG = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 DEBUG_TB_PROFILER_ENABLED = True
-DEBUG_TB_TEMPLATE_EDITOR_ENABLED = False
-SECRET_KEY = '395c494289c0f7909336c53feac4079f6d7760ed763269b4'
-UPLOAD_FOLDER = basedir+'/websitemixer/static/upload/'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'])
+DEBUG_TB_TEMPLATE_EDITOR_ENABLED = True
+SECRET_KEY = 'websitemixersupersecretkey1234567890'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir,'myapp.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
