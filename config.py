fb90a8f373c8217beacca7b4be0577e4a01c77d4
bderstine <brad@bizzartech.com>
2018-02-08 19:34:23 +0000

    update base and login with bootstrap 4

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
-DEBUG = True
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DEBUG = False
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 DEBUG_TB_PROFILER_ENABLED = True
-DEBUG_TB_TEMPLATE_EDITOR_ENABLED = True
-SECRET_KEY = 'websitemixersupersecretkey1234567890'
+DEBUG_TB_TEMPLATE_EDITOR_ENABLED = False
+SECRET_KEY = 'fb44917aa467dbb2b18149957b776454e8e841225ab61fd8'
+UPLOAD_FOLDER = basedir+'/websitemixer/static/upload/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'])
 
+SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(basedir,'myapp.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
