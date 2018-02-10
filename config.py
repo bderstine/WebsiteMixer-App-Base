9efb5d934e970a6499f2b764de009f9c07ef9839
bderstine <brad@bizzartech.com>
2018-02-10 07:40:23 +0000

    add Psycopg to requirements

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 DEBUG_TB_PROFILER_ENABLED = True
-DEBUG_TB_TEMPLATE_EDITOR_ENABLED = True
-SECRET_KEY = 'websitemixersupersecretkey1234567890'
+DEBUG_TB_TEMPLATE_EDITOR_ENABLED = False
+SECRET_KEY = '6f9ce79437e3fd1e11cdba78198a31b4fb9225b84efe39cc'
+UPLOAD_FOLDER = basedir+'/websitemixer/static/upload/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip'])
 
+SQLALCHEMY_DATABASE_URI = 'postgresql://webm:monkey216@localhost:5432/webm_dev'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
