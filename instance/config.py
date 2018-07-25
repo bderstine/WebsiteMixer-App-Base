d083f0cb69fbd78c057fdc88c59e318e7ff6a32e
Brad <brad@bizzartech.com>
2018-07-25 01:04:30 +0000

    reset config.py

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,3 @@
-import os
-from flask import current_app
-
 DEBUG = True
 DEBUG_TB_INTERCEPT_REDIRECTS = False
 DEBUG_TB_PROFILER_ENABLED = True
@@ -9,5 +6,3 @@ SECRET_KEY = 'websitemixersupersecretkey1234567890'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-
-DATABASE = '/srv/WebsiteMixer-App-Base/instance/websitemixer.sqlite'
--- websitemixer/__init__.py
+++ websitemixer/__init__.py
@@ -7,12 +7,15 @@ def create_app(test_config=None):
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__, instance_relative_config=True)
     #app.config.from_pyfile('config.py', silent=True)
+
+    app.config.from_mapping(
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, 'websitemixer.sqlite'),
+    )
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        #app.config.from_pyfile('config.py', silent=True)
         app.config.from_pyfile('config.py')
-        print(app.config)
-        #print(app.config['DATABASE'])
     else:
         # load the test config if passed in
         app.config.update(test_config)
